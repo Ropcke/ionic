c1b7376829e49f47c1eb86bbf9ae73f99ff5d440
Bruno Röpcke <56569928+Ropcke@users.noreply.github.com>
2022-07-22 13:43:20 -0300

    final da aula

--- testapp/src/app/tab2/tab2.page.ts
+++ testapp/src/app/tab2/tab2.page.ts
@@ -9,4 +9,20 @@ export class Tab2Page {
 
   constructor() {}
 
+  doarAlimentos () {
+    this.abrirUrl ('https://www.google.com/maps/search/?api=1&query=Ponto+Doacao+Alimento');
+  }
+
+  doarRoupas () {
+    this.abrirUrl ('https://www.google.com/maps/search/?api=1&query=Ponto+Doacao+Roupas');
+  }
+
+  doarSangue () {
+    this.abrirUrl ('https://www.google.com/maps/search/?api=1&query=Ponto+Doacao+Sangue');
+  }
+
+  abrirUrl (url : string) {
+    window.open(url, '_blank').focus();
+  }
+
 }
